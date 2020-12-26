62311c51747ed814acfc860c05fa0e8d8d962e39
LucaDL <lanzellottiluca9800@gmail.com>
2020-12-26 19:02:29 +0100

    Fix-Move

--- Team-3/Assets/Move.cs
+++ Team-3/Assets/Move.cs
@@ -8,8 +8,11 @@ public class Move : MonoBehaviour
     public Transform cam;
 
     float Speed = 3f;
+    private bool Rotazione = false;
+   
 
     public float TimeR = 0.1f;
+    public float Timerr;
     public float Velocità;
 
     private Vector3 Direzione;
@@ -17,6 +20,7 @@ public class Move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Timerr = TimeR;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -27,19 +31,27 @@ public class Move : MonoBehaviour
         float Movez = Input.GetAxis("Vertical");
 
         Direzione = new Vector3(Movex, 0, Movez);
-
+        
         if(Direzione.magnitude >= 0.1)
         {
+            //Timerr = TimeR;
             float targetAngle = Mathf.Atan2(Direzione.x, Direzione.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float Angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref Velocità, TimeR);
+            float Angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref Velocità, Timerr);
             transform.rotation = Quaternion.Euler(0f, Angle, 0f);
+            Rotazione = true;
         }
        
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(transform.position + (Direzione * Speed* Time.fixedDeltaTime));
+        if (Rotazione == true)
+        {
+            //Timerr = 1f;
+            rb.MovePosition(transform.position + (Direzione * Speed * Time.fixedDeltaTime));                  
+        }
+        
+       
     }
 
 
