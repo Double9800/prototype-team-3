0d390d32eeb6fa5869bbf972fa5387263c1cbe78
Ivan <ivantummolo@gmail.com>
2020-12-26 20:01:58 +0100

    Shooting System

---
+++ Team-3/Assets/Scripts/Shoot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject canna;
+    [SerializeField] GameObject projectiles;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, canna.transform.position, canna.transform.rotation, projectiles.transform);
+        }
+    }
+}
