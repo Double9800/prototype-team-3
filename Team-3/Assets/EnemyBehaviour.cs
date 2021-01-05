6fd9ab8939592b791b7062f5f19643bd304c58d6
Ivan <ivantummolo@gmail.com>
2021-01-05 18:46:21 +0100

    AlessioMerge

--- Team-3/Assets/EnemyBehaviour.cs
+++ Team-3/Assets/EnemyBehaviour.cs
@@ -34,7 +34,7 @@ public class EnemyBehaviour : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             myScript.enabled = false;
-            
+
         }
     }
 
--- Team-3/Assets/ShootEnemy.cs
+++ Team-3/Assets/ShootEnemy.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ShootEnemy : MonoBehaviour
 {
@@ -9,25 +7,24 @@ public class ShootEnemy : MonoBehaviour
     [SerializeField]
     private Transform canna;
     [SerializeField]
-    private Transform enemyParent;
-    [SerializeField]
     private float maxTimer;
     [SerializeField]
     private float Timer;
+    [SerializeField] GameObject projectiles;
 
 
 
 
 
-    // Start is called before the first frame update
+
     void Start()
     {
         Timer = maxTimer;
     }
 
-    // Update is called once per frame
+
     void Update()
-    {              
+    {
         Shoot();
     }
 
@@ -36,7 +33,7 @@ public class ShootEnemy : MonoBehaviour
         Timer -= Time.deltaTime;
         if (Timer <= 0)
         {
-            Instantiate(enemyBullet, canna);
+            Instantiate(enemyBullet, canna.transform.position, canna.transform.rotation, projectiles.transform);
             Timer = maxTimer;
         }
     }
